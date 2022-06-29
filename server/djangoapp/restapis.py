82d83fb5fc295fb1a65471a89444569cb500e5ba
Thales <thaleshgl36.com>
2022-06-29 20:29:57 +0000

    added get_reviews proxy

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,13 +1,38 @@
 import requests
 import json
-# import related models here
+from .models import CarDealer# import related models here
 from requests.auth import HTTPBasicAuth
 
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
 #                                     auth=HTTPBasicAuth('apikey', api_key))
+def get_request(url, **kwargs):
+    
+    # If argument contain API KEY
+    api_key = kwargs.get("api_key")
+    print("GET from {} ".format(url))
+    try:
+        if api_key:
+            params = dict()
+            params["text"] = kwargs["text"]
+            params["version"] = kwargs["version"]
+            params["features"] = kwargs["features"]
+            params["return_analyzed_text"] = kwargs["return_analyzed_text"]
+            response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
+                                    auth=HTTPBasicAuth('apikey', api_key))
+        else:
+            # Call get method of requests library with URL and parameters
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
 
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 # Create a `post_request` to make HTTP POST requests
 # e.g., response = requests.post(url, params=kwargs, json=payload)
@@ -17,7 +42,49 @@ from requests.auth import HTTPBasicAuth
 # def get_dealers_from_cf(url, **kwargs):
 # - Call get_request() with specified arguments
 # - Parse JSON results into a CarDealer object list
+def get_dealers_from_cf(url, **kwargs):
+    results = []
+    state = kwargs.get("state")
+    if state:
+        json_result = get_request(url, state=state)
+    else:
+        json_result = get_request(url)
 
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["body"]["rows"]["docs"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+
+            #dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"],
+                                    full_name=dealer["full_name"], id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
+                                   short_name=dealer["short_name"], st=dealer["st"], zip=dealer["zip"])
+            results.append(dealer_obj)
+
+    return results
+
+#get dealer by Id
+def get_dealer_by_id(url, id):
+    json_result = get_request(url, id=id)
+
+    if json_result:
+        # Get the row list in JSON as dealers
+        dealers = json_result["body"]["rows"]["docs"]
+        # For each dealer object
+        for dealer in dealers:
+            # Get its content in `doc` object
+
+            #dealer_doc = dealer["doc"]
+            # Create a CarDealer object with values in `doc` object
+            dealer_obj = CarDealer(address=dealer["address"], city=dealer["city"],
+                                    full_name=dealer["full_name"], id=dealer["id"], lat=dealer["lat"], long=dealer["long"],
+                                   short_name=dealer["short_name"], st=dealer["st"], zip=dealer["zip"])
+            results.append(dealer_obj)
+
+    return dealer_obj
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
 # def get_dealer_by_id_from_cf(url, dealerId):
